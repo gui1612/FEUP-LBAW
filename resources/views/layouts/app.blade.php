410af8045fed5fe7c7592c7465832fc2997d9f0e
André Lima <me@limwa.pt>
2022-11-18 00:08:29 +0000

    chore: organize things a bit better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,61 +8,10 @@
 @endsection
 
 @section('body')
-  <header>
-    @include('partials.header')
-  </header>
+  @include('partials.header')
   <main>
     @yield('content')
   </main>
   @yield('extra')
-  <footer>
-    @include('partials.footer')
-  </footer>
+  @include('partials.footer')
 @endsection
-
-{{-- <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>
-      @hasSection ('title')
-        @yield('title') | {{ config('app.name', 'Laravel') }}
-      @else
-        {{ config('app.name', 'Laravel') }}
-      @endif
-    </title>
-        
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script type="text/javascript">
-        // Fix for Firefox autofocus CSS bug
-        // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-    </script>
-    <script type="text/javascript" src={{ asset('js/app.js') }} defer>
-</script>
-  </head>
-  <body>
-    <header>
-      @yield('header')
-      {{-- <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
-      @if (Auth::check())
-      <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-      @endif --}}
-    {{--</header>
-    <main>
-      <section id="content">
-        @yield('content')
-      </section>
-      @yield('extra')
-    </main>
-    <footer>
-      @yield('footer')
-    </footer>
-  </body>
-</html> --}}
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,25 @@
-<nav class="headernav">
-    <h2>
-        <a class="headernav_logo">
-            <img src= {{ asset('images/logo.svg') }} alt="Wrottit logo" width="60" height="60">
-            <span>Wrottit</span>
-        </a>
-    </h2>
-
-    <div class="headernav_navigation">
-        <div>
-            <a class="headernav_navigation_new_post">
-                <img src= {{ asset('images/icons/plus.svg') }} alt="Create a post" width="30" height="30">
+<header>
+    <nav class="headernav">
+        <h2>
+            <a class="headernav_logo">
+                <img src= {{ asset('images/logo.svg') }} alt="Wrottit logo" width="60" height="60">
+                <span>Wrottit</span>
             </a>
+        </h2>
+    
+        <div class="headernav_navigation">
+            <div>
+                <a class="headernav_navigation_new_post">
+                    <img src= {{ asset('images/icons/plus.svg') }} alt="Create a post" width="30" height="30">
+                </a>
+            </div>
+    
+            <div>
+                <a class="headernav_navigation_user_profile">
+                    <img src= {{ asset('images/logo.svg') }} alt="My profile" width="60" height="60">
+                </a>
+            </div>
         </div>
     
-        <div>
-            <a class="headernav_navigation_user_profile">
-                <img src= {{ asset('images/logo.svg') }} alt="My profile" width="60" height="60">
-            </a>
-        </div>
-    </div>
-
-</nav>
+    </nav>
+</header>
