bc1e459b6a098edb45cb65ccca4de64937e5f010
André Lima <me@limwa.pt>
2022-11-19 23:20:55 +0000

    chore: remove untracked folder

--- website/storage/framework/views/f76486ec34f185bb749864d770a952824ed5cb4b.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-  <p><?php echo e($card); ?></p>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/marinevas/uni/LBAW/lbaw2264/website/resources/views/users/index.blade.php ENDPATH**/ ?>
