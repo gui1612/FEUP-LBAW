5ce514d366af953946d544a4ec048c0250ddb74a
CarlosMealha <up202005954@edu.fc.up.pt>
2022-11-21 22:57:27 +0000

    feat: added edit profile button to profile page

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -9,8 +9,13 @@
             <div id="profile-picture" class="ratio ratio-1x1 border border-3 rounded-circle border-white position-absolute bottom-0" style="left: 50%; width: clamp(7.5rem, 15vw, 12.5rem); transform: translate(-50%, 50%)">
                 <img src="{{ $user->profile_picture }}" alt="{{ $user->username }}'s profile picture" width="30" height="30" class="rounded-circle position-absolute">
             </div>
-        </div>
-        <div class="d-flex align-items-center p-4 justify-content-center">
+        </div>   
+        <div class="d-flex align-items-center p-4 justify-content-center position-relative">
+            <div class="position-absolute top-0 end-0 translate-middle">
+                <a class="text-decoration-none text-secondary" href=" {{ route('post.create') }} ">
+                     Edit Profile<i class='bi bi-arrow-right"'></i>
+                </a>
+            </div> 
             <div class="d-flex flex-column p-4 gap-2 align-items-center">
                 <span>{{ '@' . $user->username }} <i class="bi bi-dot"></i> {{ $user->posts()->get()->count() }} posts</span>
                 <span> <i class="bi bi-stars"></i> {{ $user->reputation }} reputation points</span>
