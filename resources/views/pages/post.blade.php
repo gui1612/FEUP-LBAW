f525d37f2270a602a5e219cb7fc2324fb57a705f
Guilherme Almeida <me@gui1612.com>
2022-11-22 17:51:56 +0000

    feat: adds warning modal

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -49,15 +49,41 @@
                 <li>
                     <a href="{{ route('post.edit', ['id'=>$id]) }}" class="dropdown-item">Edit</a>
                 </li>
-                <li><a class="dropdown-item text-danger" href="{{ route('feed') }}">Delete</a></li>
+                <!-- Button trigger modal -->
+                <li><type="button" class="btn btn-primary dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Delete</a></li>
             </ul>
-        </div>
-        
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this post?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                        <form method="POST" action="{{ route('post.delete', $post->id)}}">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger" type="submit">Yes</button>
+                        </form>
 
+                </div>
+                </div>
+            </div>
+            </div>
+
+
+            </div>
+        
         @include('partials.post_content')
 
         <span class="my-2"> on {{ date_format($post->created_at, 'Y-m-d') }}</span>
         
         @include('partials.rating')
     </article>
+    
 @endsection
