f21f04fc0a1b3eb2a973a1721547469b6b5cc270
CarlosMealha <up202005954@edu.fc.up.pt>
2022-11-22 19:24:40 +0000

    fix: redirect path on register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/team';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
