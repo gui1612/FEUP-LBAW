652c29d7490198a67564126c64b32e421acdc743
marineve17 <mariana.lobao2002@gmail.com>
2022-11-23 17:22:27 +0000

    likes changing colour

--- resources/views/partials/rating.blade.php
+++ resources/views/partials/rating.blade.php
@@ -1,9 +1,11 @@
 <div class="post_content_rating d-flex align-items-center">
-    <button class="btn btn-lg" style="on"> 
-        <i class="bi bi-hand-thumbs-up"></i>
+    <button class="btn btn-lg wt-like-click" style="on"> 
+        <i class="bi bi-hand-thumbs-up unfilled"></i>
+        <i class="bi bi-hand-thumbs-up-fill like_filled"></i>
     </button>
     {{ $post->rating }} 
-    <button class="btn btn-lg"> 
-        <i class="bi bi-hand-thumbs-down"></i>
+    <button class="btn btn-lg wt-like-click"> 
+        <i class="bi bi-hand-thumbs-down unfilled"></i>
+        <i class="bi bi-hand-thumbs-down-fill dislike_filled"></i>
     </button>
 </div>
