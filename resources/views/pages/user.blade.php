0dcdba65dce1823d252c7db879b5535d8e103769
marineve17 <mariana.lobao2002@gmail.com>
2022-12-02 12:16:26 +0000

    fix profile page

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -4,33 +4,52 @@
 @php($paginator = $user->posts()->visible()->paginate(10))
 
 @section('content')
-    <div class="d-flex flex-column bg-white container m-3 px-0">
-        <div id="user-info" class="position-relative" style="margin-bottom: clamp(1.5rem, 5vw, 4rem);">
-            <div id="banner-picture" class="bg-primary" style="min-height: 12.5rem;">
-                @if($user->banner_picture_url())
-                    <img src="{{ $user->banner_picture_url() }}" alt="{{ $user->username }}'s banner picture" width="100" height="500" class="w-100" style="height: clamp(12.5rem, 25vw, 20rem); object-fit: cover">
-                @endif
-            </div>
-            <div id="profile-picture" class="ratio ratio-1x1 border border-3 rounded-circle border-white position-absolute bottom-0" style="left: 50%; width: clamp(7.5rem, 15vw, 12.5rem); transform: translate(-50%, 50%)">
-                <img src="{{ $user->profile_picture_or_default_url() }}" alt="{{ $user->username }}'s profile picture" width="30" height="30" class="rounded-circle position-absolute">
-            </div>
-        </div>   
-        <div class="d-flex align-items-center p-4 justify-content-center position-relative">
-            @can('edit', $user)    
-            <div class="position-absolute top-0 end-0 translate-middle">
-                <a href="{{ route('user.edit', ['user'=>$user]) }}" class="btn btn-secondary text-decoration-none text-white">
-                     Edit Profile<i class='bi bi-arrow-right"'></i>
-                </a>
-            </div> 
-            @endcan
-            <div class="d-flex flex-column p-4 gap-2 align-items-center">
-                <span>{{ '@' . $user->username }} <i class="bi bi-dot"></i> {{ $paginator->total() }} posts</span>
-                <span> <i class="bi bi-stars"></i> {{ $user->reputation }} reputation points</span>
-                <div id="user_bio">
-                    <p class="d-flex text-center">{{ $user->bio }}</p>
+    <div class="d-flex container m-3 px-0">
+
+        <section class="vh-100" style="background-color: #eee;">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center h-100">
+                    <div class="container rounded bg-white p-4" style="height: min-content">
+                        <div class="card-body text-center" style="width: min-content">
+                            <div class="mt-3 mb-4 d-flex flex-column align-items-center" style="height: 16vh">
+                                <img src=" {{ $user->banner_picture }}" alt="{{ $user->username . '\'s banner picture' }}" 
+                                class="img-fluid" style="width: 100%; height: 75%; object-fit: cover;">
+                                <img src=" {{ $user->profile_picture_or_default_url() }}" alt="{{ $user->username . '\'s banner picture' }}"
+                                class="rounded-circle img-fluid position-absolute" style="border: solid white 2px; width: 100px; top: 27%;">
+                            </div>
+                            <h4 class="mb-2"> {{ $user->first_name . ' ' . $user->last_name }} </h4>
+                            <p class="text-muted mb-4"> {{ '@' . $user->username }} <span class="mx-2"></span> </p>
+
+                            @if($user->id == Auth::user()->id)
+                                <button type="button" class="btn btn-primary">
+                                    Edit Profile
+                                </button>
+                            @else 
+                                <button type="button" class="btn btn-primary">
+                                    Follow
+                                </button>
+                            @endif
+                            <div class="d-flex justify-content-between text-center mt-4 mb-2">
+                                <div>
+                                <p class="mb-2 h5"> {{ $user->reputation }} </p>
+                                <p class="text-muted mb-0">Reputation Points</p>
+                                </div>
+                                <div class="px-3">
+                                <p class="mb-2 h5"> {{ $paginator->total() }} </p>
+                                <p class="text-muted mb-0">Posts</p>
+                                </div>
+                                <div>
+                                <p class="mb-2 h5"> xx </p>
+                                <p class="text-muted mb-0">Followers</p>
+                                </div>
+                            </div>
+                            <p class="my-3"> {{ $user->bio }} </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+
         <div class="d-flex flex-column align-items-center">
             @foreach($paginator->items() as $post)
                 @include('partials.post_preview', ['on_profile'=>True])
