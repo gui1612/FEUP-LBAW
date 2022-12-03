f3ce1eaf3de6d0eb70de54c5ca196d331143b8b8
CarlosMealha <up202005954@edu.fc.up.pt>
2022-12-03 15:06:13 +0000

    feat: about page done

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 class StaticPagesController extends Controller
 {
     public function showAboutPage() {
-      view('pages.static.about');
+      return view('pages.static.about');
     }
 
     public function showContactPage() {
-      view('pages.static.contact');
+      return view('pages.static.contact');
     }
 
     public function showFeaturesPage() {
-      view('pages.static.features');  
+      return view('pages.static.features');  
     }
 }
--- resources/views/pages/static/about.blade.php
+++ resources/views/pages/static/about.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Post')
+@section('title', 'About Us')
 
 @section('content')
-<h2>Hello!</h2>
-
+<div class="container w-75 m-4 bg-white pt-5 px-5 py-5 d-flex flex-column gap-2 justify-content-start">
+  <h3>About Us</h3>
+  <div class="horizontalDivider mb-3"></div>
+  <div class="col-md-11 px-4">
+    <p class="p-0 m-0">
+      Wrottit is platform developed by a group of students
+      in LEIC utmost care. Our goal is for your experience
+      to be the best possible.
+      Our goal is for your experience
+      to be the best possible.
+    </p>
+    <p>
+      We want to give you a possibility
+      to see all the world's news in a single platform, while 
+      having the freedom to post whatever you want and connect
+      with people alike
+    </p>
+  </div>
+</div>
 @endsection
