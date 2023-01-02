4cf3b0946569d60d33f55bef9ed6ccc5ff26847d
André Lima <me@limwa.pt>
2023-01-02 02:23:20 +0000

    feat: search improvements

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller {
 
@@ -16,9 +15,9 @@ class SearchController extends Controller {
             ->orderByRaw("ts_rank(tsvector, plainto_tsquery('english', ?)) DESC", [$search])
             ->paginate(20);
 
-
         $users = User::whereRaw("tsvector @@ plainto_tsquery('english', ?)", [$search])
             ->orderByRaw("ts_rank(tsvector, plainto_tsquery('english', ?)) DESC", [$search])
+            ->union(User::where('username', $search))
             ->paginate(20);
 
         return view('pages.search', ['query' => $search, 'users' => $users, 'posts' => $posts]);
--- resources/views/partials/searchbar.blade.php
+++ resources/views/partials/searchbar.blade.php
@@ -1,6 +1,8 @@
 <form method="GET" action="/search" class="d-flex align-items-center position-relative">
-    <input type="text" name="q" placeholder="Search" class="form-control ms-2" style="width: min-content">
-    <button id="search-button" type="submit" class="btn btn-primary position-absolute" style="right: 0">
-        <i class="bi bi-search"></i>
-    </button>
+    <div class="input-group">
+        <input type="text" class="form-control" name="q" placeholder="Search" aria-label="Search">
+        <button id="search-button" type="submit" class="btn btn-primary">
+            <i class="bi bi-search"></i>
+        </button>
+    </div>
 </form>
