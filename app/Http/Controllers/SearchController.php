10a1a3576cca57848d9279b7e94260eaed1a0782
André Lima <me@limwa.pt>
2023-01-02 02:42:53 +0000

    feat: add fts for forums

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Forum;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -20,6 +21,10 @@ class SearchController extends Controller {
             ->union(User::where('username', $search))
             ->paginate(20);
 
-        return view('pages.search', ['query' => $search, 'users' => $users, 'posts' => $posts]);
+        $forums = Forum::whereRaw("tsvector @@ plainto_tsquery('english', ?)", [$search])
+        ->orderByRaw("ts_rank(tsvector, plainto_tsquery('english', ?)) DESC", [$search])
+        ->paginate(20);
+
+        return view('pages.search', ['query' => $search, 'users' => $users, 'posts' => $posts, 'forums' => $forums]);
     }
 }
