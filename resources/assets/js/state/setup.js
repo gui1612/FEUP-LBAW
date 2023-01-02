b261212f66a087d6438964e0fefb846f21d4b347
André Lima <me@limwa.pt>
2023-01-02 03:09:01 +0000

    feat: improve signals

--- resources/assets/js/state/setup.js
+++ resources/assets/js/state/setup.js
@@ -3,9 +3,29 @@ import { signal } from './signals';
 export function setupState() {
     document.body.querySelectorAll('[data-wt-signal]').forEach((container) => {
         const signalName = container.getAttribute('data-wt-signal');
+        if (signalName == null) return;
+
+        const signalInfomation = signalName.split(':');
+
+        let name, attribute = null;
+        if (signalInfomation.length === 1) {
+            [name] = signalInfomation;
+        } else if (signalInfomation.length === 2) {
+            [name, attribute] = signalInfomation;
+        } else {
+            console.warn(`Invalid signal name ${signalName}, expected format is "name" or "name:attribute"`);
+            return;
+        }
+
         let value = container.getAttribute('data-wt-value');
 
         const { subscribe } = signal(signalName, value);
-        subscribe((v) => container.textContent = v.toString());
+        subscribe((v) => {
+            if (attribute) {
+                container.setAttribute(attribute, v.toString());
+            } else {
+                container.textContent = v.toString();
+            }
+        });
     });
 }
