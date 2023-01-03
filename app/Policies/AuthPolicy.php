c8828ed34793d1d006f4dd9cf60c55205b3acf58
Guilherme Almeida <me@gui1612.com>
2023-01-03 19:09:10 +0000

    feat: implements block users

--- app/Policies/AuthPolicy.php
+++ app/Policies/AuthPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class AdminPolicy
@@ -10,7 +11,8 @@ class AdminPolicy
     use HandlesAuthorization;
 
     public function login(User $user) {
-        if 
+        if (!$user->is_blocked)
+            return Response::denyWithStatus(403, 'This account is block');
         return true;
     }
 }
